6f6da7d379e0f7f8a46af14678fdbac487058e13
sivamohanreddy <psmohan.net@gmail.com>
2016-10-10 19:02:30 +0530

    Add files via upload

---
+++ Add_new_hotel.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnsavehotel_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["HappyHolidaysConn"].ConnectionString.ToString());
+        SqlCommand cmd = new SqlCommand("insert_hotel", con);
+        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@hname", txthotelname.Text);
+        cmd.Parameters.AddWithValue("@r", optrating.SelectedItem.Value);
+        cmd.Parameters.AddWithValue("@rpd", txt_rentperday.Text);
+        cmd.Parameters.AddWithValue("@hcity", txt_cityname.Text);
+        
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Session["addhotel"] = txthotelname.Text.ToString();
+        Response.Redirect("Add_newhotel_success.aspx");
+
+    }
+}
